c3e8eee3fea00cfd34d2be89bf80eb16a7eb5dc8
ShivamMauryaShambhu <sm6496514@gmail.com>
2024-05-20 00:45:29 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,1 +1,10 @@
+# my first program in python
+print("Helloworld")
 
+# practice-->1
+# write a program to input two numbers $ print their sum....
+#solution.......
+s=int(input("enter your first number"))
+m=int(input("enter your second number"))
+sum=a+b
+print("a+b=",sum)
